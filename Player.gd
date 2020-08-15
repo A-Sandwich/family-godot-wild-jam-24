2c939b4682c154b73f1c058deb4bacd7011ecaa7
Kyle <kbburkholder@gmail.com>
2020-08-15 22:43:52 +0100

    Tilemap is it's own scene, started level 1

--- Player.gd
+++ Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-const GRAVITY = 1200.0
-const WALK_SPEED = 200
-const JUMP_SPEED = 400
+const GRAVITY = 1800.0
+const WALK_SPEED = 500
+const JUMP_SPEED = 600
 var velocity = Vector2()
 
 var jump_time = 0.0
@@ -16,6 +16,7 @@ func _physics_process(delta):
 	else:
 		velocity.x = 0
 		
-	move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+	#move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+	move_and_slide(velocity, Vector2(0, -1))
 	if is_on_floor() and Input.is_action_pressed("jump"):
 		velocity.y = -JUMP_SPEED
