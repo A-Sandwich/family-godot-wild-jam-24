689eb5aee4062a5f3903a2d24f86abc1114be86e
Kyle Burkholder <kbburkholder@gmail.com>
2020-08-22 12:22:16 -0400

    more progress!

--- Kitten.gd
+++ Kitten.gd
@@ -1,10 +1,10 @@
-extends KinematicBody2D
+extends RigidBody2D
 
 const GRAVITY = 1800.0
 const WALK_SPEED = 500
 const JUMP_SPEED = 350
 var velocity = Vector2()
-
+var jump = false
 var jump_time = 0.0
 
 signal kitten_location
@@ -14,27 +14,18 @@ func _ready():
 	self.connect("kitten_location", player, "_on_kitten_location")
 	emit_signal("kitten_location", position)
 
-func _physics_process(delta):
-	velocity.y += delta  * GRAVITY
-	
-	var current_velocity = move_and_slide(velocity, Vector2.UP, true, 4, 1.22173)
-	var slides = get_slide_count()
-	if slides:
-		deal_with_slope(slides)
-	if is_on_floor() and Input.is_action_pressed("jump"):
+func _integrate_forces(state):
+	if Input.is_action_pressed("jump"):
 		$Timer.start()
-
-func deal_with_slope(number_of_slides):
-	for slide in range(number_of_slides):
-		var collision = get_slide_collision(slide)
-		if collision.collider.name == "Player":
-			pass
-		if is_on_floor() and collision.normal.y < 1.0 and velocity.x != 0.0:
-			velocity.y = collision.normal.y
-
+	if jump:
+		var lv = state.get_linear_velocity()
+		lv.y =  -500
+		state.set_linear_velocity(lv)
+		#state.add_force(position, Vector2(position.x, position.y -10))
+		jump = false
 
 func _on_Timer_timeout():
-	velocity.y = -JUMP_SPEED
+	jump = true
 	$Timer.stop()
 
 func _on_kitten_held():
